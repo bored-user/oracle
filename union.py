9d2200a5407093b8be39c3577982030db6cab99d
João <joao.guimaraes.alves.vaz@gmail.com>
2021-02-10 09:33:10 -0300

    Add generator-module file

---
+++ union.py
@@ -0,0 +1,21 @@
+import itertools
+from PIL import Image
+import uuid
+
+def union(width: int, height: int, black_and_white: bool = False):
+    for c in itertools.product(itertools.product(range(256) if black_and_white else range(0, 256, 255), repeat=3), repeat=(width * height)):
+        c = c[::-1]
+
+        img = Image.new('RGB', (width, height))
+        _map = img.load()
+
+        for x in range(width):
+            for y in range(height):
+                _map[x, y] = c[y + width * x]
+
+        yield img
+
+if __name__ == '__main__':
+    for i in union(1000, 1000):
+        i.save(str(uuid.uuid4()) + '.jpg')
+
